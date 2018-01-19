dd639074a5e81f0da8678f004445521c7019ae95
Musa Ugurlu <mugurlu@rivercityscience.org>
2018-01-19 16:55:18 -0500

    Initial Commit

--- MediaPlayer/MediaPlayer.psm1
+++ MediaPlayer/MediaPlayer.psm1
@@ -1,6 +1,131 @@
-<#
-	My Function
-#>
-function Get-Function {
+class Media 
+{
+	[string] $FullName
+	[string] $Path
+	[string] $Type
+	[string] $Status
+	[int]    $Duration
+	[int]    $Order
+	
+	Media() 
+	{
 
+	}
+
+	[void] Play()
+	{
+
+	}
+
+	[void] Pause()
+	{
+
+	}
+
+	[void] Stop()
+	{
+		
+	}
+
+
+}
+
+class Playlist {
+	[Media[]]  $Media
+	[string]   $FolderLocation
+	[PlayMode] $Mode
+
+	Playlist()
+	{
+
+	}
+
+	[void] Play()
+	{
+
+	}
+
+	[void] Pause()
+	{
+
+	}
+
+	[void] Stop()
+	{
+		
+	}
+
+	[Media] Current()
+	{
+		return ''
+	}
+}
+
+enum PlayMode {
+	Sequential = 1
+	Shuffle = 2
+}
+
+function New-Playlist {
+	[CmdletBinding()]
+	param (
+		
+	)
+	
+	begin {
+	}
+	
+	process {
+	}
+	
+	end {
+	}
+}
+
+function Get-Playlist {
+	[CmdletBinding()]
+	param (
+		
+	)
+	
+	begin {
+	}
+	
+	process {
+	}
+	
+	end {
+	}
+}
+
+function Start-Playlist {
+	[CmdletBinding()]
+	param (
+		
+	)
+	
+	begin {
+	}
+	
+	process {
+	}
+	
+	end {
+	}
+}
+
+function Stop-Playlist {
+	[CmdletBinding()]
+	param (
+		
+	)
+	
+	begin {
+	}
+	
+	process {
+	}
+	
+	end {
+	}
 }
---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Media Player
+still under development
