0d1e85cfeb906455026191d284e6eed2dd43c8b8
Musa Ugurlu <musaugurlu@outlook.com>
2018-01-19 22:09:13 -0500

    Initial Commit

--- MediaPlayer/MediaPlayer.psm1
+++ MediaPlayer/MediaPlayer.psm1
@@ -1,131 +1,233 @@
-class Media 
-{
-	[string] $FullName
-	[string] $Path
-	[string] $Type
-	[string] $Status
-	[int]    $Duration
-	[int]    $Order
+class Media {
+    [string]      $FullName
+    [string]      $Path
+    [string]      $Type
+    [MediaStatus] $Status = [MediaStatus]::NotPlaying
+    [int]         $Duration
+    [int]         $Order
+    [bool]        $Loop = 0
 	
-	Media() 
-	{
+    Media() {
 
-	}
+    }
 
-	[void] Play()
-	{
+    [void] Play() {
 
-	}
+    }
 
-	[void] Pause()
-	{
+    [void] Pause() {
 
-	}
+    }
 
-	[void] Stop()
-	{
+    [void] Stop() {
 		
-	}
+    }
 
 
 }
 
 class Playlist {
-	[Media[]]  $Media
-	[string]   $FolderLocation
-	[PlayMode] $Mode
+    [int]      $Id
+    [string]   $Name
+    [Media[]]  $Media
+    [string]   $Path
+    [PlayMode] $Mode = [PlayMode]::Sequential
 
-	Playlist()
-	{
+    Playlist() {
 
-	}
+    }
 
-	[void] Play()
-	{
+    [void] Play() {
 
-	}
+    }
 
-	[void] Pause()
-	{
+    [void] Pause() {
 
-	}
+    }
 
-	[void] Stop()
-	{
+    [void] Stop() {
 		
-	}
+    }
 
-	[Media] Current()
-	{
-		return ''
-	}
+    [Media] Current() {
+        return ''
+    }
+}
+
+enum MediaStatus {
+    NotPlaying
+    NowPlaying
+    Paused
 }
 
 enum PlayMode {
-	Sequential = 1
-	Shuffle = 2
+    Sequential
+    Shuffle
 }
 
 function New-Playlist {
-	[CmdletBinding()]
-	param (
-		
-	)
+    [CmdletBinding()]
+    param (
+        # Name
+        [Parameter(Mandatory = $true)]
+        [string] $Name,
+
+        # Folder Location
+        [Parameter(Mandatory = $false)]
+        [string] $FolderLocation
+    )
 	
-	begin {
-	}
+    begin {
+        Write-Verbose "Checking if Playlist.list file is exist in `"$($env:TEMP + "\LuckyMediaPlayer")`" folder"
+        if (Test-Path -Path "$env:TEMP\LuckyMediaPlayer\Playlists.list") {
+			
+            Write-Verbose "The file exists. Checking if the name `"$Name`" is given to another playlist"
+            $Playlist = Import-Csv -Path "$env:TEMP\LuckyMediaPlayer\Playlists.list"
+            if ($Playlist | Where-Object {$_.Name -eq $Name}) {
+                Write-Error "Invalid Name. Another playlist is already associated with this name `"$Name`""
+                break
+            }
+
+            Write-Verbose "The name `"$Name`" is valid"
+        }
+        else {
+            Write-Verbose "The file Playlist.list is not exist. Creating a new one in $($env:TEMP + "\LuckyMediaPlayer") folder."
+            New-Item -Path "$env:TEMP\LuckyMediaPlayer\" -Name "Playlists.list" -ItemType File -Force -Confirm:$false | Out-Null
+        }
+    }
 	
-	process {
-	}
+    process {
+        Write-Verbose 'Checking if $FolderLocation is given'
+        if ([string]::IsNullOrEmpty($FolderLocation)) {
+            Write-Verbose '$FolderLocation is not given. Empty playlist is being created.'
+			New-EmptyPlaylist($Name)
+			
+        } else {
+			if (Test-Path -Path $FolderLocation) {
+				$AllMedia = Get-ChildItem -Path $FolderLocation -Recurse -Include *.mp3,*.wav
+
+				if ($AllMedia.FullName.count -gt 0) {
+					
+				} else {
+					Write-Verbose "The folder given has no media files. Empty playlist will be created"
+					New-EmptyPlaylist($Name)
+				}
+
+			} else {
+				Write-Error "Invalid folder path `"$FolderLocation`"."
+				break
+			}
+		}
+    }
 	
-	end {
-	}
+    end {
+    }
 }
 
 function Get-Playlist {
-	[CmdletBinding()]
-	param (
-		
-	)
+    [CmdletBinding()]
+    param (
+        # Name
+        [Parameter(Mandatory = $false)]
+        [string] $Name
+    )
 	
-	begin {
-	}
+    begin {
+    }
 	
-	process {
-	}
+    process {
+        if (Test-Path -Path "$env:TEMP\LuckyMediaPlayer\Playlists.list") {
+            $PlayLists = Import-Csv -Path "$env:TEMP\LuckyMediaPlayer\Playlists.list"
+            if ($PlayLists.name.count -gt 0) {
+                foreach ($PlayList in $PlayLists) {
+                    $NewPlaylist = [Playlist]::new()
+                    $NewPlaylist.Id = $PlayList.Id
+                    $NewPlaylist.Name = $PlayList.Name
+
+                    Write-Output $NewPlaylist
+                }
+            }
+            else {
+                $NewPlaylist = [Playlist]::new()
+                Write-Output $NewPlaylist
+            } 
+        }
+        else {
+            $NewPlaylist = [Playlist]::new()
+            Write-Output $NewPlaylist
+        }
+    }
 	
-	end {
-	}
+    end {
+    }
 }
 
 function Start-Playlist {
-	[CmdletBinding()]
-	param (
+    [CmdletBinding()]
+    param (
 		
-	)
+    )
 	
-	begin {
-	}
+    begin {
+    }
 	
-	process {
-	}
+    process {
+    }
 	
-	end {
-	}
+    end {
+    }
 }
 
 function Stop-Playlist {
-	[CmdletBinding()]
-	param (
+    [CmdletBinding()]
+    param (
 		
-	)
+    )
 	
-	begin {
-	}
+    begin {
+    }
 	
-	process {
-	}
+    process {
+    }
 	
-	end {
+    end {
+    }
+}
+
+function Add-ToPlaylist {
+    [CmdletBinding()]
+    param (
+        # Playlist
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [Playlist] $PlaylistName,
+		
+        # File Path
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string] $FilePath
+    )
+	
+    begin {
+    }
+	
+    process {
+    }
+	
+    end {
+    }
+}
+
+function New-EmptyPlaylist ($Name) {
+	$NewPlaylist = [Playlist]::new()
+	$NewPlaylist.Id = ((Get-Playlist | Sort-Object -Property Id | Select-Object -Last 1 -Property Id)[0].Id + 1)
+	$NewPlaylist.Name = $Name
+
+	Write-Verbose 'New playlist is being written in to playlist.list'
+	try {
+		$NewPlaylist | Export-Csv -Path "$env:TEMP\LuckyMediaPlayer\Playlists.list" -NoTypeInformation -Append
 	}
+	catch {
+		Write-Error "Couldn't write in `"$($env:TEMP + '\LuckyMediaPlayer\Playlists.list')`". Make sure you have sufficient permission to write in the folder`"."
+	}
+
+	return $NewPlaylist
 }
